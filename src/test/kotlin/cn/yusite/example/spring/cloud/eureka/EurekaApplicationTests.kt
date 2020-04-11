d9951f64b24f17e59b09c1f55443288d9ba5eba4
sjie <ligongdamao@163.com>
2020-04-11 16:18:28 +0800

    init project

---
+++ src/test/kotlin/cn/yusite/example/spring/cloud/eureka/EurekaApplicationTests.kt
@@ -0,0 +1,13 @@
+package cn.yusite.example.spring.cloud.eureka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
